88b775722f939825abfad9993b855f2db0c8d2d7
James Attard <attaj002@mfsa.internal>
2017-01-30 17:19:01 +0100

    Replace twitch URL with proxy and fixed ajax calls

--- js/index.js
+++ js/index.js
@@ -3,13 +3,7 @@ function getStreamers() {
   var twitchChannels =
     [
       "ESL_SC2",
-      "OgamingSC2",
-      "cretetion",
       "freecodecamp",
-      "storbeck",
-      "habathcx",
-      "RobotCaleb",
-      "noobs2ninjas",
       "doesnotexist1"
     ];
 
@@ -17,48 +11,54 @@ function getStreamers() {
       streamStatus,
       logo;
 
-  twitchChannels.map(function(i) {
-    streamerName = i;
+  twitchChannels.map(function(channel) {
     $.ajax({ // query channel info (determine if username exists)
-      url: "https://api.twitch.tv/kraken/channels/" + i,
-      headers: {
-        'Client-ID': '821w445od23f4yucok8wjoxzcc7773'
-      },
+      // url: "https://api.twitch.tv/kraken/streams/" + channel,
+      // headers: {
+      //   'Client-ID': '821w445od23f4yucok8wjoxzcc7773'
+      // },
+      url: "https://wind-bow.gomix.me/twitch-api/streams/" + channel + '?callback=?',
+      dataType: 'json',
       success: function(res) {
-        //console.log(res);
-        logo = res.logo;
+        var game,
+            status;
+        if (res.stream == null) {
+          game = "Offline";
+          status = "offline";
+        } else {
+          game = res.stream.game;
+          status = "online";
+        };
+
         $.ajax({ // query stream info (get stream status)
-          url: "https://api.twitch.tv/kraken/streams/" + i,
-          headers: {
-            'Client-ID': '821w445od23f4yucok8wjoxzcc7773'
-          },
+          // url: "https://api.twitch.tv/kraken/channels/" + channel,
+          // headers: {
+          //   'Client-ID': '821w445od23f4yucok8wjoxzcc7773'
+          // },
+          url: "https://wind-bow.gomix.me/twitch-api/channels/" + channel + '?callback=?',
+          dataType: 'json',
           success: function(res) {
-            description = res.stream == null ? 'Offline' : res.stream.channel.status;
-            $('#streamersAll').append('<p>' +
-              '<img src="' + logo + '" height="50" width="50">' +
-              i + ': ' + description + '</p>'); // refresh results
-          },
-          error: function(error){
-            console.log(error);
-          }
-        }); // end of .ajax() "stream info"
+            //console.log(res);
+            //console.log(game, status);
+            var logo = res.logo != null ? res.logo : "https://dummyimage.com/50x50&text=X",
+              name = res.display_name != null ? res.display_name : channel,
+              description = status === "online" ? ': ' + res.status : "",
+              html = '<div class="row ' +
+              status + '"><div class="col-xs-2 col-sm-1" id="icon"><img src="' +
+              logo + '" class="logo" height="50" width="50"></div><div class="col-xs-10 col-sm-3" id="name"><a href="' +
+              res.url + '" target="_blank">' +
+              name + '</a></div><div class="col-xs-10 col-sm-8" id="streaming">'+
+              game + '<span class="hidden-xs">' +
+              description + '</span></div></div>';
+            console.log(html);
+            status === "online" ? $("#streamerList").prepend(html) : $("#streamerList").append(html);
 
-        //$('#streamerList').text(''); // refresh results
-      },
-      error: function(error){
-        if (error.status == 404){
-          logo = 'https://unsplash.it/50/50?image=2';
-          description = 'Not found';
-          $('#streamersAll').append('<p>' +
-            '<img src="' + logo + '" height="50" width="50">' +
-            i + ': ' + description + '</p>');
-        }
-      }
-    }); // end of .ajax() "channel info"
-
-  }); // end of twitchChannels.map()
-
-} // end of function getStreamers(status)
+            } // end of ajax success call #2 function
+          }); // end of ajax call #2
+        } // end of ajax success call #1 function
+      }); // end of ajax call #1
+    }); // end of map array
+  }; // end of getStreamers()
 
 $(document).ready(function() {
   getStreamers();
